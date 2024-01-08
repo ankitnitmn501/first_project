d2f6c08f412b60fa5f4a4bb38202392e362f08af
avichal <avichal@gmail.com>
2024-01-08 06:09:02 +0000

    multiplication code done

--- newfile.py
+++ newfile.py
@@ -1,3 +1,6 @@
 a = int(input("Enter first number")
 b = int(input("Enter second number")
 print(a-b)# Subtraction
+a = int(input("Enter the first number")
+b = int(input("Enter the second number")
+print(a*b) # Multiplication code
